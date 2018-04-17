ad01286d0213062bc5952da2fef2a106fe3c80cc
Andrew Wall <andrew.wall@vivint.com>
2018-04-16 18:36:18 -0600

    Working bookshelf solution

--- bookshelf/bookshelf.py
+++ bookshelf/bookshelf.py
@@ -36,13 +36,12 @@ ci.close()
 
 print("Shelves: " + str(len(shelves)))
 print("Books: " + str(len(books)))
+
 # How many Shelves
 total_shelves = 0
 
 c = 0
 for shelf in shelves:
-	print("books: " + str(len(books)))
-	print("shelf: " + str(shelf))
 	if len(books) <= 0:
 		break
 	total_shelves += 1
@@ -50,35 +49,15 @@ for shelf in shelves:
 	total_books = len(books)
 	i = 0
 	while True:
-		print("i:" + str(i))
-		print("tb:" + str(total_books))
-		if shelf < books[i]:
+		if shelf > books[i]:
 			shelf -= books[i]
 			books.remove(books[i])
 			total_books -= 1
 		else:
 			i += 1
 
-		if i < total_books:
+		if i >= total_books:
 			break
-
-
-#for shelf in shelves:
-#	if len(books) <= 0:
-#		break
-#	if len(shelves) <= 0:
-#		impossible = 1
-#		break
-#	total_shelves += 1
-#
-#	while shelf > 0:
-#		for i in range(0,len(books)):
-#			if books[i] < shelf:
-#				shelf -= book
-#				books.remove(book)
-#				break
-#		shelves.remove(shelf)
-#		break
 
 if len(books) == 0:
 	print("Shelves Used: " + str(total_shelves))
